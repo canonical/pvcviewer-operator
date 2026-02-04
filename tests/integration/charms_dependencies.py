64317308d14f1531b1c33365539522d95898115c
Michal Hucko <michal.hucko@canonical.com>
2026-02-04 12:11:17 +0100

    Fix typo in channel names for CharmSpec
    Signed-off-by: Michal Hucko <michal.hucko@canonical.com>

--- tests/integration/charms_dependencies.py
+++ tests/integration/charms_dependencies.py
@@ -3,11 +3,11 @@
 from charmed_kubeflow_chisme.testing import CharmSpec
 
 ISTIO_GATEWAY = CharmSpec(
-    charm="istio-gateway", channel="1.24/stbale", config={"kind": "ingress"}, trust=True
+    charm="istio-gateway", channel="1.24/stable", config={"kind": "ingress"}, trust=True
 )
 ISTIO_PILOT = CharmSpec(
     charm="istio-pilot",
-    channel="1.24/stbale",
+    channel="1.24/stable",
     config={"default-gateway": "kubeflow-gateway"},
     trust=True,
 )
